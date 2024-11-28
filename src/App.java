83ac10cddcf26d6e9b72be70aac04f0d974a22a9
Guakocius <engelhardt.alexander@mein.gmx>
2024-11-28 16:56:48 +0100

    Update

--- src/App.java
+++ src/App.java
@@ -11,6 +11,9 @@ public class App extends JFrame {
 
     public App() {
 
+        JTable stundenplan = new JTable();
+        stundenplan.setLayout(new GridLayout(14, 5));
+
         setTitle("LSF");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(500, 500);
@@ -19,27 +22,42 @@ public class App extends JFrame {
          currentMonday = LocalDate.now().with(DayOfWeek.MONDAY);
 
         panel = new JPanel();
-        panel.setLayout(new GridLayout(1, 5, 10, 10));
-        updateWeek();
+        panel.setLayout(new GridLayout(1, 6, 10, 10));
+        JPanel zeit = new JPanel();
+        zeit.setLayout(new GridLayout(15, 1, 10, 10));
+
+
+        for(int i = 7; i <= 19; i++) {
+            while(i == 7) {
+                zeit.add(new JLabel("Zeit"));
+                zeit.add(new JLabel("vor 8"));
+                i++;
+            }
+            zeit.add(new JLabel("" + i));
+        }
+        zeit.add(new JLabel("ab 20"));
 
         JButton prevWeek = new JButton("←");
-        prevWeek.addActionListener(e -> {
+        prevWeek.addActionListener(_ -> {
             currentMonday = currentMonday.minusWeeks(1);
             updateWeek();
         });
 
 
         JButton nextWeek = new JButton("→");
-        nextWeek.addActionListener(e -> {
+        nextWeek.addActionListener(_ -> {
             currentMonday = currentMonday.plusWeeks(1);
             updateWeek();
         });
 
         add(prevWeek, BorderLayout.WEST);
         add(panel, BorderLayout.CENTER);
+        add(zeit, BorderLayout.WEST);
         JScrollPane scrollPane = new JScrollPane(panel);
         add(scrollPane, BorderLayout.CENTER);
         add(nextWeek, BorderLayout.EAST);
+        add(stundenplan, BorderLayout.SOUTH);
+        updateWeek();
 
     }
 
@@ -50,8 +68,10 @@ public class App extends JFrame {
 
         for (int i = 0; i < tage.length; i++) {
             LocalDate date = currentMonday.plusDays(i);
+
             JLabel label = new JLabel(tage[i] + ", " + date.format(formatter));
             label.setHorizontalAlignment(SwingConstants.CENTER);
+
             panel.add(label);
         }
         panel.revalidate();
